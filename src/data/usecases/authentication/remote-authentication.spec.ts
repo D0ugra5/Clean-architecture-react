41f6a755a1aa164196d874ac17dfffb0413d355f
Douglas Sousa <douglasmb87@gmail.com>
2022-09-18 01:41:31 -0300

    refactor: move HttpClientSpy to its own flie

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,8 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
+import { HttpClientSpy } from "../../test/mock-htpp-client";
 import { RemoteAuthentication } from "./remote-authentication";
 
 describe("RemoteAuthentication", () => {
   it("Should call httpClient with correct url ", async () => {
-    class HttpClientSpy implements HttpPostClient {
-      url?: string;
-      async post(url: string): Promise<void> {
-        this.url = url;
-        return Promise.resolve();
-      }
-    }
     const url = "dougUrl";
     const httpPostClientSpy = new HttpClientSpy();
     const sut = new RemoteAuthentication(url, httpPostClientSpy);
